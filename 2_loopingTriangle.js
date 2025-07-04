b6eaa3e4f34101d1e091e77d1350285b13cb852f
nishthapnt <nishthapnt@gmail.com>
2025-07-04 14:40:26 +0530

    add 2_loopingtriangle.js

---
+++ 2_loopingTriangle.js
@@ -0,0 +1,20 @@
+//printing a triangle pattern using loops
+
+
+let str= "#";
+for(i=1;i<8;i++){
+    console.log(str);
+    for(j=1;j<=1;j++){ //j<=1 this is right. dont do j<=8...thats dumb
+        str=str+"#"
+    }
+
+}
+//wuhuuuu i did it
+
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
