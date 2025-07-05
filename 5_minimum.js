9777a39b3082bb0dbe0983b1e0b7f33b56d27d93
nishthapnt <nishthapnt@gmail.com>
2025-07-05 16:16:44 +0530

    added 5_minimum.js

---
+++ 5_minimum.js
@@ -0,0 +1,22 @@
+function minimum(arr){
+    let min = arr[0];
+    if (arr.length==0){
+        return null;
+    }
+    else if(arr.length==1){
+        return arr[0];
+    }
+    
+    else{
+        
+        for(i=1; i<arr.length; i++){
+            if(arr[i]<min){
+                min = arr[i];
+            }
+        }
+    } 
+    return min;      
+}
+
+array=[3, 5, 1, 8, 2];
+console.log(minimum(array)); // Output: 1
