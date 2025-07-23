9f5f71d5044ab05c5afa39a41846c22b5f751bea
nishthapnt <nishthapnt@gmail.com>
2025-07-23 20:07:37 +0530

    changes to 16

--- 16_asunc.js
+++ 16_asunc.js
@@ -12,3 +12,26 @@ function print(){
 }
 setTimeout(print,100);
 console.log(sum(1000000));
+
+
+// 1. Call stack: setTimeout(print, 100) → Web API timer started
+// 2. Call stack: console.log(sum(1000000)) → runs sum() completely
+// 3. After sum() completes and stack is clear → event loop checks the Task Queue
+// 4. Timer (print) callback is now pushed to call stack and executed
+
+
+// because sum() takes long to complete, setTimeout(print, 100) will not execute exactly after 100ms. 
+// It will wait for the call stack to be empty, which happens only after sum() finishes
+
+//-----------------------------------------------------------------------------------------------------
+
+// so asynchronous is basically non sequential execution based on the execution time of tasks
+// (done by the programmer by using call backs) and is not same as multithreading
+
+// Asynchronous means:
+// "Don't wait here. Move on, and I'll come back to this task later when it's ready."
+// This is not about running things in parallel, it's about not blocking the thread.
+
+// In multithreading, two or more tasks truly run at the same time using multiple CPU cores.
+// In JavaScript, only one thing runs at a time, but asynchronous tasks are managed smartly via the event loop.
+
